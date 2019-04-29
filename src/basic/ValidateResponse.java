d161549abb528c93a0d9337cee2924d91cd23a3e
alves-fabio <fabio.alves@gmail.com>
2019-04-29 00:09:50 -0300

    Commit - aula 36 - v2

--- src/basic/ValidateResponse.java
+++ src/basic/ValidateResponse.java
@@ -15,9 +15,9 @@ public class ValidateResponse {
 		RestAssured.baseURI = "https://restcountries-v1.p.rapidapi.com";
 		RestAssured.basePath = "/name";
 	}
-
+	
 	@Test()
-	public void statusCodeVerification() {
+	public void printResponse() {
 		Response res =
 		given()
 			.header("X-RapidAPI-Host", "restcountries-v1.p.rapidapi.com")
@@ -26,6 +26,24 @@ public class ValidateResponse {
 			.get("/brazil");
 		
 		System.out.println(res.body().prettyPrint());
+	}
+	
+
+	@Test()
+	public void responseVerification() {
+		given()
+			.header("X-RapidAPI-Host", "restcountries-v1.p.rapidapi.com")
+			.header("X-RapidAPI-Key", "47d3e1ebe6msh876aeb3cb1fb5bcp1bc94cjsn392bd45ffb8d")
+		.when()
+			.get("/brazil")
+		.then()
+			.statusCode(200)
+			.body("[0].capital", equalTo("Brasília"))
+			.body("[0].region", equalTo("Americas"))
+			.body("[0].subregion", equalTo("South America"))
+			.body("[0].population", equalTo(204772000))
+			.body("[0].area", equalTo(8515767.0f))
+			.contentType(ContentType.JSON);
 	}
 
 }
